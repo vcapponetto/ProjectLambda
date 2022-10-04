406f2be1a5e3d5912cb80687bc052f1bcd6778ee
vcapponetto <vcapponetto@gmail.com>
2022-10-04 18:04:46 -0300

    commit de orden en la máquina tablet

--- FunctionalProgramming/FunctionalProgramming/src/com/functionalProgramming/EjercicioPredicate/Funciones.java
+++
@@ -1,9 +0,0 @@
-package com.functionalProgramming.EjercicioPredicate;
-
-import java.util.function.BiFunction;
-
-public class Funciones {
-    public Double incrementoSalario(Empleado emp, Double incremento, BiFunction<Double, Double, Double> bi) {
-        return bi.apply(emp.getSalario(), incremento);
-    }
-}
